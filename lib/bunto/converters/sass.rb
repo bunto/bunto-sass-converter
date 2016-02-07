2bc106cb306bc2658633a5b44f72d52a8c22b4c7
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2016-02-07 15:07:11 +0100

    Create sass.rb

---
+++ lib/bunto/converters/sass.rb
@@ -0,0 +1,21 @@
+require 'sass'
+require 'bunto/utils'
+require 'bunto/converters/scss'
+
+module Bunto
+  module Converters
+    class Sass < Scss
+      safe true
+      priority :low
+
+      def matches(ext)
+        ext =~ /^\.sass$/i
+      end
+
+      def syntax
+        :sass
+      end
+    end
+  end
+end
+j
